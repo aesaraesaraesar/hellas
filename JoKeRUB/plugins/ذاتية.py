547dc12c6abd0f67e71c98414e36d8f722e98897
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-02-09 00:32:19 +0300

    Update ذاتية.py

--- JoKeRUB/plugins/ذاتية.py
+++ JoKeRUB/plugins/ذاتية.py
@@ -1,1 +1,116 @@
-#تم حذف الذاتية لأسباب ....
+from telethon import events
+import random, re
+from JoKeRUB.utils import admin_cmd
+import asyncio 
+from JoKeRUB import l313l
+from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+import os
+import datetime
+from JoKeRUB import *
+Aljoker_Asbo3 = {
+    'Monday': 'الاثنين',
+    'Tuesday': 'الثلاثاء',
+    'Wednesday': 'الأربعاء',
+    'Thursday': 'الخميس',
+    'Friday': 'الجمعة',
+    'Saturday': 'السبت',
+    'Sunday': 'الأحد'
+}
+@l313l.on(admin_cmd(pattern="(جلب الصورة|جلب الصوره|ذاتيه|ذاتية)"))
+async def dato(event):
+    if not event.is_reply:
+        return await event.edit("..")
+    lMl10l = await event.get_reply_message()
+    pic = await lMl10l.download_media()
+    await bot.send_file(
+        "me",
+        pic,
+        caption=f"""
+- تـم حفظ الصـورة بنجـاح ✓
+  """,
+    )
+    await event.delete()
+@l313l.on(admin_cmd(pattern="(الذاتية تشغيل|ذاتية تشغيل)"))
+async def reda(event):
+    if gvarstatus ("savepicforme"):
+        return await edit_delete(event, "**᯽︙حفظ الذاتيات مفعل وليس بحاجة للتفعيل مجدداً **")
+    else:
+        addgvar("savepicforme", "reda")
+        await edit_delete(event, "**᯽︙تم تفعيل ميزة حفظ الذاتيات بنجاح ✓**")
+@l313l.on(admin_cmd(pattern="(الذاتية تعطيل|ذاتية تعطيل)"))
+async def Reda_Is_Here(event):
+    if gvarstatus ("savepicforme"):
+        delgvar("savepicforme")
+        return await edit_delete(event, "**᯽︙تم تعطيل حفظت الذاتيات بنجاح ✓**")
+    else:
+        await edit_delete(event, "**᯽︙انت لم تفعل حفظ الذاتيات لتعطيلها!**")
+def joker_unread_media(message):
+    return message.media_unread and (message.photo or message.video)
+async def Hussein(event, caption):
+    media = await event.download_media()
+    sender = await event.get_sender()
+    sender_id = event.sender_id
+    lMl10l_date = event.date.strftime("%Y-%m-%d")
+    lMl10l_day = Aljoker_Asbo3[event.date.strftime("%A")]
+    await bot.send_file(
+        "me",
+        media,
+        caption=caption.format(sender.first_name, sender_id, lMl10l_date, lMl10l_day),
+        parse_mode="markdown"
+    )
+    os.remove(media)
+@l313l.on(events.NewMessage(func=lambda e: e.is_private and joker_unread_media(e) and e.sender_id != bot.uid))
+async def Reda(event):
+    if gvarstatus("savepicforme"):
+        caption = """**
+           ♡    ♡
+♡ تم حفظ الذاتية بنجاح ✓
+♡ أسم المرسل : [{0}](tg://user?id={1})
+♡  تاريخ الذاتية : `{2}`
+♡  أرسلت في يوم `{3}`
+       ♡        ♡
+        **"""
+        await Hussein(event, caption)
+        
+# Wespr File by  @F_O_1
+# Copyright (C) 2021 JoKeRUB TEAM
+@borg.on(
+    admin_cmd(pattern="لايك ?(.*)")
+)
+async def wspr(event):
+    if event.fwd_from:
+        return
+    l313lb = event.pattern_match.group(1)
+    rrrd7 = "@like"
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    tap = await bot.inline_query(rrrd7, l313lb) 
+    await tap[0].click(event.chat_id)
+    await event.delete()
+    
+@borg.on(admin_cmd("م27"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("᯽︙ اوامر الهمسه واكس او \n\n⌔︙الامر  • `.همسة`\n⌔︙الاستخدام  • لكتابة همسه سرية لشخص في المجموعه \n\n᯽︙ الامر • `.الهمسة`\n᯽︙ استخدامه • لعرض كيفية كتابة همسة سرية\n\n᯽︙ الامر • `.اكس او `\n ᯽︙ استخدامه • ففط ارسل الامر لبدء لعبة اكس او\n\n᯽︙ CH  - @k_jj_j")
+        
+@borg.on(admin_cmd("الهمسة"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("**᯽︙ شـرح كيـفية كـتابة همـسة سـرية**\n᯽︙ اولا اكتب الامر  .همسة  بعدها الرسالة بعدها اكتب معرف الشخص\n᯽︙ مـثال  :   `.همسة ههلا @F_O_1`")
+        
+@borg.on(
+    admin_cmd(
+       pattern="اكس او$"
+    )
+)
+# كتابة وتعديل فريق 7rB   #@F_O_1
+async def gamez(event):
+    if event.fwd_from:
+        return
+    jmusername = "@xoBot"
+    uunzz = "play"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(jmusername, uunzz)
+    await tap[0].click(event.chat_id)
+    await event.delete()

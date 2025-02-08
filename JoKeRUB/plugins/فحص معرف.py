a616cb0e6ce2a6f99aade1869730f053e8b55c7c
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-02-09 01:33:59 +0300

    Update فحص معرف.py

--- JoKeRUB/plugins/فحص معرف.py
+++ JoKeRUB/plugins/فحص معرف.py
@@ -8,7 +8,7 @@ plugin_category = "البوت"
 # تهيئة Instaloader
 loader = Instaloader()
 
-@l313l.on(events.NewMessage(pattern='.فحص (.*)'))
+@l313l.on(events.NewMessage(pattern='.معرف فحص (.*)'))
 async def checknft(event):
     try:
         chat_id = event.chat_id

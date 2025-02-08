26bcff9ffec67c4e908043c8a9c5c24fea40e0cd
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-02-08 22:12:32 +0300

    Update الانتحال.py

--- JoKeRUB/plugins/الانتحال.py
+++ JoKeRUB/plugins/الانتحال.py
@@ -24,7 +24,7 @@ DEFAULTUSER = str(AUTONAME) if AUTONAME else str(ALIVE_NAME)
 DEFAULTUSERBIO = (
     str(DEFAULT_BIO)
     if DEFAULT_BIO
-    else "الحمد لله دائماً وابداً 🎀 𝑆𝐻 : @jepthon"
+    else "الحمد لله دائماً وابداً 🎀 𝑆𝐻 : @HELLASUserBot"
 )
 
 @l313l.ar_cmd(pattern="انتحال(?:\s|$)([\s\S]*)")

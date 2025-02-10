5786a460588c7a21fa23116eb4afffe05aba355d
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-02-10 13:43:48 +0300

    Update فحص.py

--- JoKeRUB/plugins/فحص.py
+++ JoKeRUB/plugins/فحص.py
@@ -87,10 +87,10 @@ async def amireallyalive(event):
 
 
 temp = """{ALIVE_TEXT}
-**‎{EMOJI}‌‎𝙽𝙰𝙼𝙴 𖠄 {mention}** ٫
-**‌‎{EMOJI}‌‎𝙿𝚈𝚃𝙷𝙾𝙽 𖠄 `{pyver}`** ٫
-**‌‎{EMOJI}‌‎𝙹𝙾𝙺𝙴𝚁 𖠄 `{telever}`** ٫
-**‌‎{EMOJI}‌‎𝚄𝙿𝚃𝙸𝙼𝙴 𖠄 `{uptime}`** ٫
-‌‎**{EMOJI}‌‎‌‎𝙿𝙸𝙽𝙶 𖠄 `{ping}`** ٫
-‌‎**{EMOJI}‌‎‌‎𝚂𝙴𝚃𝚄𝙿 𝙳𝙰𝚃𝙴 𖠄 `{Tare5}`** ٫
+**‎{EMOJI}‌‎NAME • {mention}** ٫
+**‌‎{EMOJI}‌‎PYTHON • `{pyver}`** ٫
+**‌‎{EMOJI}‌‎HELLAS • `{telever}`** ٫
+**‌‎{EMOJI}‌‎UBTIME • `{uptime}`** ٫
+‌‎**{EMOJI}‌‎‌‎PINK • `{ping}`** ٫
+‌‎**{EMOJI}‌‎‌‎SETUP DATA • `{Tare5}`** ٫
 **𖠄 𝐇𝐞𝐥𝐥𝐚𝐬 𝘂𝘀𝗲𝗿𝗯𝗼𝘁 𖠄**"""

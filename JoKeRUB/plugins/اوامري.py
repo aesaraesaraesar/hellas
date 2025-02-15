1f74f9a7803daec702e21193c3070f5ce1dbbc15
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-02-15 09:57:29 -0800

    Update اوامري.py

--- JoKeRUB/plugins/اوامري.py
+++ JoKeRUB/plugins/اوامري.py
@@ -51,8 +51,7 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
                 [
                     Button.inline(" الأكستـرا ", data="iiers"),
                     Button.inline(" الانتحال والتقليد ", data="uscuxrz"),
-                ]
-                [Button.inline("𝐒𝐨𝐮𝐫𝐜𝐞 𝐇𝐞𝐥𝐥𝐚", )],
+                ],
             ]
             if JEP_IC and JEP_IC.endswith((".jpg", ".png", "gif", "mp4")):
                 result = builder.photo(

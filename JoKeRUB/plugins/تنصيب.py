4697cb2188b2019c80b71e50adf9e9d428b46e88
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-02-15 13:04:51 -0800

    Update تنصيب.py

--- JoKeRUB/plugins/تنصيب.py
+++ JoKeRUB/plugins/تنصيب.py
@@ -1,35 +1,33 @@
 from telethon import events, Button
 from ..Config import Config
-from ..sql_helper.globals import gvarstatus
 from l313l.razan.resources.mybot import *
 
-ROZ_PIC = ""
-
-if Config.TG_BOT_USERNAME is not None and tgbot is not None:
+if Config.TG_BOT_USERNAME and tgbot is not None:
     @tgbot.on(events.InlineQuery)
     async def inline_handler(event):
         builder = event.builder
-        result = None
-        query = 
-        await bot.get_me()
+        query = event.text.lower()  
+
         if query.startswith("السورس") and event.query.user_id == bot.uid:
-            buttons = [[Button.url("1- شرح التنصيب", "https://youtu.be/ATAgbLGzr7w"), Button.url("2- استخراج ايبيات", "https://my.telegram.org/"),],[Button.url("3- ستخراج تيرمكس", "https://replit.com/@lMl10l/CodeMusic?lite=1&outputonly=1"), Button.url("4- بوت فاذر", "http://t.me/BotFather"),],[Button.url("5- رابط التنصيب", "https://heroku.com/deploy?template=https://github.com/HuReiq/lMl10l"),],[Button.url("المطـور 👨🏼‍💻", "https://t.me/lMl10l"),]]
-            if ROZ_PIC and ROZ_PIC.endswith((".jpg", ".png", "gif", "mp4")):
-                result = builder.photo(ROZ_PIC, text=ROZ, buttons=buttons, link_preview=False)
-            elif ROZ_PIC:
-                result = builder.document(ROZ_PIC,title="JoKeRUB",text=ROZ,buttons=buttons,link_preview=False)
-            else:
-                result = builder.article(title="JoKeRUB",text=ROZ,buttons=buttons,link_preview=False)
-            await event.answer([result] if result else None)
-@bot.on(admin_cmd(outgoing=True, pattern="السورس"))
+            buttons = [
+                [Button.url("1- قناه السورس", "https://t.me/HELLASUserBot"), 
+                 Button.url("2- استخراج ايبيات", "https://my.telegram.org/")],
+                [Button.url("3- استخراج تيرمكس", "https://t.me/bothellasbot"), 
+                 Button.url("4- بوت فاذر", "http://t.me/BotFather")],
+                [Button.url("5- اسعار التنصيب", "https://t.me/HELLASUserBot/66")],
+                [Button.url("المطـور 👨🏼‍💻", "https://t.me/F_Q_1")]
+            ]
+
+            result = builder.article(title=" ", text=" ", buttons=buttons, link_preview=False)  
+            await event.answer([result])
+
+@bot.on(events.NewMessage(pattern="السورس"))  
 async def repo(event):
-    if event.fwd_from:
-        return
     TG_BOT = Config.TG_BOT_USERNAME
-    if event.reply_to_msg_id:
-        await event.get_reply_message()
     response = await bot.inline_query(TG_BOT, "السورس")
-    await response[0].click(event.chat_id)
+    
+    if response:
+        await response[0].click(event.chat_id)
+    
     await event.delete()
 
-# edit by ~ @lMl10l

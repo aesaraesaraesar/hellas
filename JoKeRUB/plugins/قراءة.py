94940d33d0bcf71cace6e225b9a9b40195236a4d
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-02-18 21:54:13 -0800

    Update قراءة.py

--- JoKeRUB/plugins/قراءة.py
+++ JoKeRUB/plugins/قراءة.py
@@ -12,52 +12,55 @@ from . import BOTLOG, BOTLOG_CHATID
 
 plugin_category = "البحث"
 
-
 @l313l.ar_cmd(
     pattern="قرائه(?:\s|$)([\s\S]*)",
     command=("قرائه", plugin_category),
     info={
-        "header":"لقرائه النص من الصوره ",
-        "الاستـخـدام": "{tr}لقرائه الصور ",
+        "header": "لقرائة النص من الصورة",
+        "الاستـخـدام": "{tr}قرائه",
     },
 )
 async def _(event):
     if event.fwd_from:
         return
+
     reply_message = await event.get_reply_message()
+    
     if not reply_message:
-        await edit_or_reply(event, "**```بالـرد على الصوره حمبـي 🧸🎈```**")
+        await edit_or_reply(event, "**بالـرد على صورة لاستخراج النص 🖼️**")
         return
-    if not reply_message.text:
-        await edit_or_reply(event, "**```بالـرد على الصوره حمبـي 🧸🎈```**")
+
+    if not reply_message.media:
+        await edit_or_reply(event, "**يجب الرد على صورة وليس على نص 🖼️❌**")
         return
+
     chat = "@Saveapostbot"
-    zzzzl1l = await edit_or_reply(event, "**╮ ❐ جـارِ القرائه ▬▭... 𓅫╰**")
+    zzzzl1l = await edit_or_reply(event, "**╮ ❐ جـارِ القراءة ▬▭... 𓅫╰**")
+
     async with event.client.conversation(chat) as conv:
         try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=6748718626)
-            )
+            response = conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
             await event.client.forward_messages(chat, reply_message)
             response = await response
             await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
             await zzzzl1l.edit(
-                "**❈╎تحـقق من انـك لم تقـم بحظـر البوت .. ثم اعـد استخدام الامـر ...🤖♥️**"
+                "**❈╎قم بإلغاء حظر البوت عبر الذهاب إلى محادثته، ثم أعد استخدام الأمر... 🤖♥️**"
             )
             return
-        if response.text.startswith(""):
-            await zzzzl1l.edit("**🤨💔...؟**")
+
+        if not response.text:
+            await zzzzl1l.edit("**🤨💔 لم أتمكن من قراءة النص من الصورة...**")
         else:
             await zzzzl1l.delete()
             await event.client.send_message(event.chat_id, response.message)
 
-
 CMD_HELP.update(
     {
-        "قرائه": "**اسم الاضافـه : **قرائه `\
-    \n\n**╮•❐ الامـر ⦂ **`.قرائه` لإرسال صورة أو معالجة رابط\
-    \n**الشـرح •• **إرسال الصورة أو معالجة الرابط الذي تم إرساله"
+        "قرائه": "**اسم الإضافة :** قرائه `\
+    \n\n**╮•❐ الأمر ⦂** `.قرائه` \
+    \n**الشـرح •• ** استخدم هذا الأمر بالرد على صورة لاستخراج النص الموجود فيها"
     }
 )
+
 

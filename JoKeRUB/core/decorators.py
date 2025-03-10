a1803dd7d5477a9f1d51477edcc78c6032d7a617
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-03-10 09:06:41 -0700

    Update decorators.py

--- JoKeRUB/core/decorators.py
+++ JoKeRUB/core/decorators.py
@@ -22,7 +22,7 @@ def check_owner(func):
         else:
             HELP_TEXT = (
                 gvarstatus("HELP_TEXT")
-                or "هذا الخيار ليس لك يجب عليك تنصيب سورس الجوكر بنفسك  @jepthon"
+                or "هذا الخيار عليك تنصيب سورس هيلاس بنفسك او راسل المطور لتنصيب تالسورس"
             )
             await c_q.answer(
                 HELP_TEXT,

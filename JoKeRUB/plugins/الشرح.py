8000c2860489bcd25e041050ab4ab079e565747a
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-03-28 09:56:03 +0300

    Update الشرح.py

--- JoKeRUB/plugins/الشرح.py
+++ JoKeRUB/plugins/الشرح.py
@@ -1,1 +1,19 @@
+from JoKeRUB import l313l
+from ..core.managers import edit_or_reply
+
+plugin_category = "البحث"
+
+@l313l.ar_cmd(
+    pattern="الشرح$",
+    command=("الشرح", plugin_category),
+    info={
+        "header": "لعرض شرح استخدام سورس هيلاس",
+        "الاستـخـدام": "{tr}الشرح",
+    },
+)
+async def _(event):
+    await edit_or_reply(
+        event, 
+        "📌 **شرح استخدام سورس هيلاس:**\n\n🔗 [اضغط هنا للمشاهدة](https://youtu.be/h0IIIkfxw30?si=9VKXcKhbBRFKlwoW)"
+    )
 
